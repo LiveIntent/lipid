9f82da6d12846b0d5ea6dab5569bc5b83e45cbc4
Phillip Markert <phillip@liveintent.com>
2024-03-20 15:54:28 -0400

    Adding v1.6 of measurement/init script

--- measurement.js
+++ measurement.js
@@ -1,73 +1,115 @@
 // ==UserScript==
-// @name         LI Measurement Script
+// @name         LI Prebid Identity Initialization and Measurement Script
 // @namespace    http://liveintent.com
-// @version      2024-03-12-1
-// @description  LiveIntent PCID Initialization and GAM Measurement Script
-// @author       You
+// @version      1.6
+// @description  Performs an a/b test for the LiveIntent prebid identity module and reports on the relative performance of each group.
+// @author       phillip@liveintent.com <Phillip Markert>
+// @homepage     https://github.com/LiveIntent/lipid
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  // start liveintent module init and measurement v1.5
-  const LI_PUBLISHER_ID = "INSERT PUBLISHER_ID HERE";
-  const LI_DISTRIBUTOR_ID = undefined;
-  const LOGGED_IN_USERS_EMAIL_OR_EMAIL_HASH = "";
+(function () {
+  /* This script sets up an a/b test for the LiveIntentId module, reporting on the relative 
+  performance of each group. The default behavior randomly selects 5% of visits to be in a 
+  CONTROL group and only enables the module for the other 95% of visits (the TREATED group). 
+  This a/b test selection logic can optionally be performed by an external process and connected 
+  here by overriding the LI_MODULE_ENABLED value to match. If the external process also handles 
+  configuring the LiveIntentId module (only when the TREATED group is selected!), then the 
+  LI_CONFIGURE_MODULE value should be set to false. */
+
+  // start liveintent module init and measurement script v1.6
+  const LI_MODULE_ENABLED = Math.random() < 0.95; // Override w/ true = TREATED, false = CONTROL
+  const LI_CONFIGURE_MODULE = true; // false if module is to be enabled by external process
   const LI_REPORTING_KEY = "li-module-enabled";
+
   const pbjs = (window.pbjs = window.pbjs || { que: [] });
   const googletag = (window.googletag = window.googletag || { cmd: [] });
-  const LI_MODULE_ENABLED = Math.random() < 0.95;
-  let bidsEnriched;
-  
-  function setTargeting(enriched, wonAll) {
-    googletag.cmd.push(function () {
-      let targeting = LI_MODULE_ENABLED ? "t1" : "t0";
-      if(enriched!==undefined) targeting += enriched ? "-e1" : "-e0";
-      if(wonAll!==undefined) targeting += wonAll ? "-wa" : "-ws";
-      googletag.pubads().setTargeting(LI_REPORTING_KEY, targeting);
-    });
-  }
-  
-  setTargeting();
-  
-  pbjs.que.push(function () {
-    if (LI_MODULE_ENABLED) {
+
+  function configureLiveIntentIdModule() {
+    if (LI_CONFIGURE_MODULE) {
       pbjs.mergeConfig({
         userSync: {
+          /* 300 is recommended for the best combination of resolution/performance.
+             If auctionDelay=0, then storage should be configured below. */
           auctionDelay: 300,
+
           userIds: [
             {
               name: "liveIntentId",
               params: {
-                publisherId: LI_PUBLISHER_ID,
-                distributorId: LI_DISTRIBUTOR_ID,
-                emailHash: LOGGED_IN_USERS_EMAIL_OR_EMAIL_HASH,
+                /* Specify either a publisherId or distributorId, but not both. */
+                // publisherId: "INSERT PUBLISHER_ID HERE",
+                // distributorId: "INSERT DISTRIBUTOR_ID HERE",
+
+                /* If available, setting this will further improve resolution.
+                   Leave as undefined, empty-string, or commented-out if not available. */
+                // emailHash: "LOGGED IN USERS EMAIL OR EMAIL HASH",
+
                 requestedAttributesOverrides: {
-                uid2: true,
-                bidswitch: true,
-                medianet: true,
-                magnite: true,
-                pubmatic: true,
-                index: true,
-                openx: true,
+                  uid2: true,
+                  bidswitch: true,
+                  medianet: true,
+                  magnite: true,
+                  pubmatic: true,
+                  index: true,
+                  openx: true,
                 },
               },
+
+              /* Storage should ONLY be configured if auctionDelay=0 */
+              // storage: {
+              //   expires: 1,
+              //   name: "pbjs_li_nonid",
+              //   type: "cookie"
+              // }
             },
           ],
         },
       });
     }
-    
-    pbjs.onEvent("auctionInit", function (args) {
-      bidsEnriched = args.adUnits && args.adUnits.some((au) => au.bids &&
-        au.bids.some((b) => b.userIdAsEids && b.userIdAsEids.some((eid) => eid.source
-        === "liveintent.com" || (eid.uids && eid.uids.some((uid) => uid.ext &&
-        uid.ext.provider === "liveintent.com")))));
-      setTargeting(bidsEnriched);
+  }
+
+  function setTargeting(enriched, wonAll) {
+    googletag.cmd.push(function () {
+      // t1 = module enabled, t0 = module disabled
+      let targeting = LI_MODULE_ENABLED ? "t1" : "t0";
+      // e1 = enriched, e0 = not enriched
+      if (enriched !== undefined) targeting += enriched ? "-e1" : "-e0";
+      // wa = Prebid won all ad-slots, ws = Prebid won some ad-slots
+      if (wonAll !== undefined) targeting += wonAll ? "-wa" : "-ws";
+      googletag.pubads().setTargeting(LI_REPORTING_KEY, targeting);
     });
-    
-    pbjs.onEvent("bidWon", function (args) {
-      setTargeting(bidsEnriched, pbjs.getAllPrebidWinningBids().length === 0);
+  }
+
+  setTargeting(); // Only reports t value at first.
+  const auctionsEnriched = {}; // Track enrichment per auctionId
+  pbjs.que.push(() => {
+    if (LI_MODULE_ENABLED) {
+      configureLiveIntentIdModule();
+    }
+
+    pbjs.onEvent("auctionInit", function (args) {
+      auctionsEnriched[args.auctionId ?? 0] = args.adUnits?.some((adUnit) =>
+        adUnit.bids?.some((bid) =>
+          bid.userIdAsEids?.some(
+            (eid) =>
+              eid.source === "liveintent.com" ||
+              eid.uids?.some((uid) => uid.ext?.provider === "liveintent.com")
+          )
+        )
+      );
+      // Reports t and e values after auctionInit event.
+      setTargeting(auctionsEnriched[args.auctionId]);
+    });
+
+    pbjs.onEvent("adRenderSucceeded", function (args) {
+      // Reports t, e, and w values after adRenderSucceeded event.
+      setTargeting(
+        auctionsEnriched[args.bid.auctionId ?? 0],
+        pbjs.getAllPrebidWinningBids().length === 0
+      );
     });
   });
-  // end liveintent module init and measurement
+  // end liveintent module init and measurement script
 })();
